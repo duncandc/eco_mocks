11b135df1599ede56df52b5d71e0a173de5ab4b9
Duncan Campbell <duncan.campbell@yale.edu>
2018-08-09 11:34:03 -0400

    fixed some pep violations

--- sham_model.py
+++ sham_model.py
@@ -10,13 +10,13 @@ from halotools.empirical_models import model_helpers as model_helpers
 from halotools.empirical_models.component_model_templates import PrimGalpropModel
 from AbundanceMatching import AbundanceFunction, calc_number_densities
 
-__author__=('Duncan Campbell')
-__all__=('DeconvolveSHAM', 'CAMGalProp')
+__author__ = ('Duncan Campbell')
+__all__ = ('DeconvolveSHAM', 'CAMGalProp')
 
 
 class DeconvolveSHAM(object):
     """
-    class to assign stellar mass, or some other primary galaxy property, 
+    class to assign stellar mass, or some other primary galaxy property,
     based on the de-convolution SHAM method, based on Yau-Yun Mao's
     implementation of Peter Behroozi's deconvolution package.
     """
@@ -36,18 +36,19 @@ class DeconvolveSHAM(object):
         stellar_mass_function : callable object
 
         prim_haloprop : string, optional
-            key indicating halo property used to assign the primary galaxy property
+            key indicating halo property used to assign the primary
+            galaxy property
 
         prim_galprop : string, optional
             key indicating primary galaxy property
-        
+
         gal_log_prop : boolean, optional
-            boolean indicating whther the scatter in the primary galaxy property
-            is modelled as a log-normal.
+            boolean indicating whther the scatter in the primary
+            galaxy property is modelled as a log-normal.
 
         gal_min_mass, gal_max_mass, gal_nbins: float, float, int
-            parameters used to bin and span galaxy abundance function. 
-            If gal_log_prop is True, these should also be logged, 
+            parameters used to bin and span galaxy abundance function.
+            If gal_log_prop is True, these should also be logged,
             and the spacing will be even in log.
 
         redshift : float
@@ -57,8 +58,8 @@ class DeconvolveSHAM(object):
         if 'redshift' in list(kwargs.keys()):
             self.redshift = kwargs['redshift']
         else:
-            self.redshift=0.0
-        
+            self.redshift = 0.0
+
         self.prim_haloprop = prim_haloprop
         self.prim_galprop = prim_galprop
         self._galprop_dtypes_to_allocate = np.dtype([(str(self.prim_galprop), 'f4')])
@@ -72,14 +73,14 @@ class DeconvolveSHAM(object):
             self._Lbox = np.inf
         # update Lbox if a halo catalog object is passed.
         self._additional_kwargs_dict = dict(inherit_halocat_properties=['Lbox'])
-        
+
         # store model parametrs
         self.gal_log_prop = gal_log_prop
         self.param_dict = ({'scatter': scatter})
         self.gal_min_mass = gal_min_mass
         self.gal_max_mass = gal_max_mass
         self.gal_nbins = gal_nbins
-        
+
         # deconvolve abundance function
         self._stellar_mass_function = stellar_mass_function
         self.deconvolve_scatter(self.param_dict['scatter'])
@@ -93,7 +94,7 @@ class DeconvolveSHAM(object):
             self._Lbox = Lbox
         except KeyError:
             print("Error automatically detecting Lbox.")
-    
+
     def deconvolve_scatter(self, scatter):
         """
         """
@@ -106,8 +107,9 @@ class DeconvolveSHAM(object):
             msample = np.linspace(self.gal_min_mass, self.gal_max_mass, self.gal_nbins)
             nsample = self._stellar_mass_function(msample)
             self.af = AbundanceFunction(msample, nsample, faint_end_first=True)
-        
+
         remainder = self.af.deconvolute(scatter, 100)
+        return remainder
 
     def assign_stellar_mass(self,  **kwargs):
         """
@@ -127,7 +129,7 @@ class DeconvolveSHAM(object):
                 Lbox = self._Lbox
 
         Lbox = np.atleast_1d(Lbox)
-        if len(Lbox)==3:
+        if len(Lbox) == 3:
             Lbox = (np.prod(Lbox))**(1.0/3.0)
         else:
             Lbox = Lbox[0]
@@ -135,7 +137,7 @@ class DeconvolveSHAM(object):
         nd_halos = calc_number_densities(table[self.prim_haloprop], Lbox)
         mstar = self.af.match(nd_halos, self.param_dict['scatter'])
 
-        if self.gal_log_prop==True:
+        if self.gal_log_prop == True:
             mstar = 10.0**mstar
             table[self.prim_galprop] = mstar
         else:
@@ -157,9 +159,10 @@ class CAMGalProp(object):
                  conditonal_rvs,
                  prim_galprop,
                  prim_galprop_bins,
-                 secondary_haloprop = 'halo_halfmass_scale',
-                 secondary_galprop = 'ssfr',
-                 rho = 1.0,
+                 prim_haloprop,
+                 secondary_haloprop='halo_halfmass_scale',
+                 secondary_galprop='ssfr',
+                 rho=1.0,
                  **kwargs):
         """
         Parameters
@@ -169,8 +172,8 @@ class CAMGalProp(object):
         if 'redshift' in list(kwargs.keys()):
             self.redshift = kwargs['redshift']
         else:
-            self.redshift=0.0
-        
+            self.redshift = 0.0
+
         self.secondary_haloprop = secondary_haloprop
         self.secondary_galprop = secondary_galprop
         self._galprop_dtypes_to_allocate = np.dtype([(str(self.secondary_galprop), 'f4')])
@@ -180,10 +183,6 @@ class CAMGalProp(object):
         # store model parametrs
         self.param_dict = ({'rho': rho})
         self.prim_galprop_bins = prim_galprop_bins
-        
-        # deconvolve abundance function
-        self._stellar_mass_function = stellar_mass_function
-        self.deconvolve_scatter(self.param_dict['scatter'])
 
     def assign_secondary_galprop(self):
         """
@@ -197,19 +196,15 @@ class HaloProps(object):
     class to carry over halo properties to galaxy mock
     """
     def __init__(self,
-                 haloprop_keys = ['halo_mpeak','halo_vpeak'],
+                 haloprop_keys=['halo_mpeak', 'halo_vpeak'],
                  **kwargs):
         """
         Parameters
         ----------
         haloprop_keys : list
         """
-        
+
         self._mock_generation_calling_sequence = []
         self._galprop_dtypes_to_allocate = np.dtype([])
         self.list_of_haloprops_needed = haloprop_keys
-
-
-
-
 
